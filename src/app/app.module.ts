891e0c03719dd65b95c81f5c557522357abb2dce
dd <dmitriy.a.drachev@gmail.com>
2019-08-26 18:00:00 +0300

    add interface Todo && autofocus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 
+import { FormsModule } from '@angular/forms';
+import { AutofocusModule } from 'angular-autofocus-fix'
+
+
 @NgModule({
   declarations: [
     AppComponent,
     TodoListComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AutofocusModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../interfaces/todo';
 
 @Component({
   selector: 'todo-list',
@@ -7,9 +8,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
+  todos: Todo[];
+  todoTitle: string;
+  idForTodo: number;
+
   constructor() { }
 
   ngOnInit() {
+    this.idForTodo = 4;
+    this.todoTitle = '';
+    this.todos = [
+        {
+          'id': 1,
+          'title': 'Finish Angular Screencast',
+          'completed': false,
+          'editing': false,
+        },
+        {
+          'id': 2,
+          'title': 'Take over world',
+          'completed': false,
+          'editing': false,
+        },
+        {
+          'id': 3,
+          'title': 'One more thing',
+          'completed': false,
+          'editing': false,
+        }
+      ];
+    }
+
+  addTodo(): void {
+    if (this.todoTitle.trim.length === 0) {
+      return;
+    }
+
+    this.todos.push( { 
+      id: this.idForTodo, 
+      title: this.todoTitle, 
+      completed: false, 
+      editing: false 
+    } )
+    
+    this.todoTitle = '';
+    this.idForTodo++;
   }
 
+  editTodo(todo: Todo): void {
+    todo.editing = true;
+  }
+
+  doneEdit(todo: Todo): void {
+    todo.editing = false;
+  }
+
+  deleteTodo(id: number): void {
+    this.todos = this.todos.filter(todo => todo.id !== id);
+  }
 }
